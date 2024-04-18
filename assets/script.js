49c6bb3eee993aa26724e8c185de7a680eb2e833
shiviravisankar <shiviravis@gmail.com>
2024-04-18 12:54:25 -0700

    many changes

--- assets/script.js
+++ assets/script.js
@@ -27,38 +27,3 @@ tabs.forEach(tab => {
     target.classList.add('active')
   })
 })
-
-// Define an array of colors
-var colors = ['#FF5EA0', '#FAD914', '#70EBE8', '#3DD4E9', '#D6E715', '#FF6D64', '#84FA67', '#E990FF'];
-
-// Function to apply randomization logic
-function applyRandomColor(element) {
-    var randomIndex = Math.floor(Math.random() * colors.length);
-    var randomColor = colors[randomIndex];
-    element.style.backgroundColor = randomColor;
-}
-
-// Apply randomization for .description a elements
-document.querySelectorAll('.description a').forEach(item => {
-    item.addEventListener('mouseenter', function() {
-        applyRandomColor(this);
-    });
-
-    item.addEventListener('mouseleave', function() {
-        this.style.backgroundColor = '';
-    });
-});
-
-// Apply randomization for .tab:hover elements
-document.querySelectorAll('.tab').forEach(item => {
-    item.addEventListener('mouseenter', function() {
-        if (this.classList.contains('hover')) {
-            applyRandomColor(this);
-        }
-    });
-
-    item.addEventListener('mouseleave', function() {
-        this.style.backgroundColor = '';
-    });
-});
-
